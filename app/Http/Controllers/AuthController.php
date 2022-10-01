0d19fa72708a5392d6961ec8d71f23a19c70beb5
Jhon Rey <jhonreymendiola@gmail.com>
2022-10-01 22:34:47 +0800

    login controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -39,4 +39,29 @@ class AuthController extends Controller
             'message' => 'Logged Out'
         ];
     }
+    public function login(Request $request){
+        $fields = $request->validate([
+            'email'=>'required|string',
+            'password'=>'required|string',
+        ]);
+
+        // check email
+        $user = User::where('email', $fields['email'])->first();
+
+        // check password
+        if(!$user || !Hash::check($fields['password'], $user->password)){
+            return response([
+                'message' => 'Bad creds'
+            ], 401);
+        }
+
+        $token = $user->createToken('myapptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token'=> $token
+        ];
+
+        return response($response, 201);
+    }
 }
