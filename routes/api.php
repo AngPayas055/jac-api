135ae04e6a31b27fee6a3a4a7f27e4050f196e0b
Jhon Rey <jhonreymendiola@gmail.com>
2022-10-01 14:54:49 +0800

    sanctum authentication first commit

--- routes/api.php
+++ routes/api.php
@@ -14,15 +14,20 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+// Route::resource('products', ProductController::class);
 
-Route::resource('products', ProductController::class);
+// public routes
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/show/{id}', [ProductController::class, 'show']);
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
-// Route::get('/products', [ProductController::class, 'index']);
-// Route::post('/products', [ProductController::class, 'store']);
-// Route::get('/products/show/{id}', [ProductController::class, 'show']);
+// protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {    
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
